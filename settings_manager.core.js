ac7e6aeab2d73c58d68eda959cf7e3e8c02d0c93
ReluctusB <endahallahan@comcast.net>
2018-05-06 16:59:54 -0500

    Update settings_manager.core.js

--- settings_manager.core.js
+++ settings_manager.core.js
@@ -11,8 +11,6 @@
 // @downloadURL  https://github.com/ReluctusB/DataFiction.net/raw/master/settings_manager.core.js
 // ==/UserScript==
 
-const setList = ["datafic-VV","datafic-FF"];
-
 function row(label, setting) {
     this.element = document.createElement("TR");
     let lab = document.createElement("TD");

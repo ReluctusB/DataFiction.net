ff2d30f80d0c917d7933c2220e30115dd62c2336
ReluctusB <endahallahan@comcast.net>
2018-05-06 14:44:47 -0500

    Create settings_manager.core.js

---
+++ settings_manager.core.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         DataFiction_settings-manager
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Manages settings for DataFiction.net
+// @author       RB
+// @match        https://www.fimfiction.net/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==

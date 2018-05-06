fc8a63d0d4a89b4563ec75ea1951411fc5ce16c3
ReluctusB <endahallahan@comcast.net>
2018-05-06 14:43:00 -0500

    Create DatFiction.user.js

---
+++ DatFiction.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         DataFiction
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  DataFiction.net is a set of userscripts that provides useful (and more esoteric) information to users of Fimfiction.net at a glance.
+// @author       RB
+// @match        https://www.fimfiction.net/*
+// @grant        none
+// @run-at       document-end
+// @require      https://github.com/ReluctusB/DataFiction.net/raw/master/fic_follower.core.js
+// @require      https://github.com/ReluctusB/DataFiction.net/raw/master/view_vote.core.js
+// @require      https://github.com/ReluctusB/DataFiction.net/raw/master/setting_manager.core.js
+// ==/UserScript==

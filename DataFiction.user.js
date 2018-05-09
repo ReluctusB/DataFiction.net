63bb32c58d771e227abead9d1e8c621a8dba9f27
ReluctusB <endahallahan@comcast.net>
2018-05-09 01:40:15 -0500

    Update DataFiction.user.js

--- DataFiction.user.js
+++ DataFiction.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         DataFiction-min
+// @name         DataFiction
 // @namespace    https://github.com/ReluctusB
 // @version      1.0
 // @description  DataFiction.net is a set of userscripts that provides useful (and more esoteric) information to users of Fimfiction.net at a glance.
